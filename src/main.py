1ab367a4fae5b3628eef70d4a12224403453dbd4
richiebailey74 <57724969+richiebailey74@users.noreply.github.com>
2023-09-04 19:34:04 -0400

    restructuring of code

---
+++ src/main.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+
+    print()
+
+    # data generation code goes here to get data sets
+
+    # create trading strategy object
+
+    # pass trading strategy object and data to backtester to generate results
+    
